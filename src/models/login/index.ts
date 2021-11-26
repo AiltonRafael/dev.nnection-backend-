789bea627322c8e28270f80814babb07c1c3e79c
Felipe Mano <manofgpa@gmail.com>
2021-11-26 10:24:53 -0300

    Add PostSchema

--- src/models/login/index.ts
+++ src/models/login/index.ts
@@ -29,31 +29,14 @@ const UserSchema = new mongoose.Schema({
 })
 
 const PostSchema = new mongoose.Schema({
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  birthday: {
-    type: Date,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  created_at: {
-    type: Date,
-    required: true,
-    default: Date.now(),
-  },
+  message: String,
+  user: Object,
+  timestamp: Date,
+  likes: Object,
+  github: String,
+  linkedin: String,
+  image: String,
+  tags: [],
 })
 
 export const User = mongoose.model('User', UserSchema)
